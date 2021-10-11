f94f9550fa567e25bb94ba58e4cc715c884045a1
Zhe Rui <zherui_tan@live.com.sg>
2021-10-11 20:36:56 +0800

    Added Login page and set to landing
    Added Login Controller (no implementation yet) and Login View

--- ShoppingCartWebApp/Startup.cs
+++ ShoppingCartWebApp/Startup.cs
@@ -50,7 +50,7 @@ namespace ShoppingCartWebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
