7fba4151834bc26b5dc873b3f84677baef262ccd
Zhe Rui <zherui_tan@live.com.sg>
2021-10-12 13:52:25 +0800

    Added Sessions to Startup

--- ShoppingCartWebApp/Controllers/LoginController.cs
+++ ShoppingCartWebApp/Controllers/LoginController.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ShoppingCartWebApp.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace ShoppingCartWebApp.Controllers
 {
     public class LoginController : Controller
     {
+        private DBContext dbContext;
+
+        public LoginController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        /*
+        public IActionResult Login(IFormCollection form)
+        {
+
+        }
+        */
     }
 }
--- ShoppingCartWebApp/Startup.cs
+++ ShoppingCartWebApp/Startup.cs
@@ -33,6 +33,8 @@ namespace ShoppingCartWebApp
                 opt.UseLazyLoadingProxies().UseSqlServer(
                     Configuration.GetConnectionString("db_conn"))
             );
+
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +57,8 @@ namespace ShoppingCartWebApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
