802393a8cc5a57d2fbc8b252dfdc38f8a384972a
CY <69207419+kcy-23@users.noreply.github.com>
2021-10-13 10:42:25 +0800

    Added MyPurchases controller & Views
    Added 2 action methods under MyPurchases controller (List; Summar)

---
+++ ShoppingCartWebApp/Controllers/MyPurchasesController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCartWebApp.Models;
+
+namespace ShoppingCartWebApp.Controllers
+{
+    public class MyPurchasesController : Controller
+    {
+        private DBContext dbContext;
+
+        public MyPurchasesController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Summary()
+        {
+            return View(); 
+        }
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
