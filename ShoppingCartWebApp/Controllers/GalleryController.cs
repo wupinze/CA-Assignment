05522aa0c0ec89202197fc440f09dd96718a0fac
Daniel <danielchua93@gmail.com>
2021-10-13 14:20:13 +0800

    get product list and pass to ViewData

--- ShoppingCartWebApp/Controllers/GalleryController.cs
+++ ShoppingCartWebApp/Controllers/GalleryController.cs
@@ -3,13 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using ShoppingCartWebApp.Models;
+using System.Diagnostics;
 namespace ShoppingCartWebApp.Controllers
 {
-    public class HomeController1 : Controller
+    public class GalleryController : Controller
     {
+        private DBContext dbContext;
+        private DB db;
+
+        public GalleryController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+            db = new DB(this.dbContext);
+        }
         public IActionResult Index()
         {
+
+            List<Product> products = db.GetProductsList();
+            foreach (var product in products)
+                Debug.WriteLine(product.ProductName);
             return View();
         }
     }
