124fd004a589a205e0918c3037015491f9bd7988
Daniel <danielchua93@gmail.com>
2021-10-16 23:16:23 +0800

    updated logout controller to make it delete session data from db using zherui's latest version

--- ShoppingCartWebApp/Controllers/LogoutController.cs
+++ ShoppingCartWebApp/Controllers/LogoutController.cs
@@ -3,17 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using ShoppingCartWebApp.Models;
 namespace ShoppingCartWebApp.Controllers
 {
     public class LogoutController : Controller
     {
+        //public IActionResult Index()
+        //{
+        //    // ask client to remove these cookies so that
+        //    // they won't be sent over next time
+        //    Response.Cookies.Delete("SessionId");
+        //    Response.Cookies.Delete("Username");
+
+        //    return RedirectToAction("Index", "Login");
+        //}
+        private DBContext dbContext;
+        private DB db;
+
+        public LogoutController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+            db = new DB(this.dbContext);
+        }
+
         public IActionResult Index()
         {
-            // ask client to remove these cookies so that
-            // they won't be sent over next time
             Response.Cookies.Delete("SessionId");
             Response.Cookies.Delete("Username");
+
+            Guid sessionId = Guid.Parse(Request.Cookies["sessionId"]);
+            Session session = dbContext.Sessions.FirstOrDefault(x =>
+                x.Id == sessionId
+            );
+
+            db.DeleteSessionData(session);
+
 
             return RedirectToAction("Index", "Login");
         }
