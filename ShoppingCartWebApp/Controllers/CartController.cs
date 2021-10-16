4f8deec5874b93e9e46d29730438697dbfd1270e
Daniel <danielchua93@gmail.com>
2021-10-17 05:00:20 +0800

    added some queries to cart table in database to cart controller

--- ShoppingCartWebApp/Controllers/CartController.cs
+++ ShoppingCartWebApp/Controllers/CartController.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCartWebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace ShoppingCartWebApp.Controllers
 {
     public class CartController : Controller
     {
+        private DBContext dbContext;
+        private DB db;
+        public CartController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+            db = new DB(this.dbContext);
+        }
         public IActionResult Index()
         {
+            if (Request.Cookies["SessionId"] != null)
+            {
+                Guid sessionId = Guid.Parse(Request.Cookies["sessionId"]);
+                Session session = dbContext.Sessions.FirstOrDefault(x =>
+                    x.Id == sessionId
+                );
+
+                List<Cart> cart = dbContext.carts.Where(x =>
+               x.user.Id == session.User.Id
+                    ).ToList();
+
+                foreach(var item in cart)
+                {
+                    Debug.WriteLine(item.product.ProductName);
+                }
+            }
             return View();
         }
     }
